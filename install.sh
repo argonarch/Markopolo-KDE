af893e87c28bea03d81508448eaafc03bbdce5e4
Argonarx <armando12123434@gmail.com>
2022-03-24 16:56:58 -0300

    install update comment

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ echo ' > Creando links'
 ln -P  bin/markopolo-randomwav /bin
 echo ' bin/markopolo-randomwav --> /bin'
 
-ln -s -r  brain/markopolo_config.py config/
-echo ' brain/markopolo_config.py --> config/'
+# ln -s -r  brain/markopolo_config.py config/
+# echo ' brain/markopolo_config.py --> config/'
 
-ln -s -r  brain/dictionary.conf config/
-echo ' brain/ --> config/'
+# ln -s -r  brain/dictionary.conf config/
+# echo ' brain/ --> config/'
 
-ln -s -r  brain/commands.conf config/
-echo ' brain/ --> config/'
+# ln -s -r  brain/commands.conf config/
+# echo ' brain/ --> config/'
